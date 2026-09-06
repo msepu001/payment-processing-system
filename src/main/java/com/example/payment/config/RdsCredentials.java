26f7ad48aa087326021bff0ca89288b278328443
Manuel Sepulveda <msepulveda7858@gmail.com>
2026-09-06 13:58:01 -0400

    RDS Ignore properties

--- src/main/java/com/example/payment/config/RdsCredentials.java
+++ src/main/java/com/example/payment/config/RdsCredentials.java
@@ -7,6 +7,10 @@ package com.example.payment.config;
      * Jackson converts the JSON secret returned by AWS into this Java record.
      */
 
+
+    import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+    @JsonIgnoreProperties(ignoreUnknown = true)
     public record RdsCredentials(
             String username,
             String password
